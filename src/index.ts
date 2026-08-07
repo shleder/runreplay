56027fb0c1ffca406e25489fb7454e80183b11ba
Shleder <maksboreichuk88@gmail.com>
2026-08-07 15:02:29 +0300

    feat: expose stable library entrypoint

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export { GithubApiError, GithubClient } from "./github.js";
+export { parseJobUrl, type ParseJobUrlOptions } from "./url.js";
+export { buildCiEvidenceBundle, type CiEvidenceBundle, type CiEvidenceSummary } from "./evidence.js";
+export { toJsonInspection, type JsonInspection } from "./json.js";
+export type { CompareOutcome, CompareReport, NoComparableBaseline } from "./compare.js";
+export type { JobReference, ResolveManifest, ResolvedJobContext } from "./types.js";
